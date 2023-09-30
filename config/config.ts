b2202761b9bcc173debb9f0bab349a9c23a744e2
nleanba <25827850+nleanba@users.noreply.github.com>
2023-09-30 12:50:45 +0200

    fixed target repo to be "-rdf"

--- config/config.ts
+++ config/config.ts
@@ -3,6 +3,6 @@ export const config = {
   sourceRepository: "plazi/treatments-xml",
   sourceRepositoryUri: "https://github.com/plazi/treatments-xml.git",
   targetBranch: "main",
-  targetRepository: "plazi/treatments-xml",
-  targetRepositoryUri: "https://github.com/plazi/treatments-xml.git",
+  targetRepository: "plazi/treatments-rdf",
+  targetRepositoryUri: "https://github.com/plazi/treatments-rdf.git",
 };
