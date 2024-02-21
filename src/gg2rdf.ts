e7ffa0a74b9f42a6fbbcf34b89a8e735d4b0f0b4
nleanba <25827850+nleanba@users.noreply.github.com>
2024-02-21 14:53:12 +0100

    fixed false self-citations

--- src/gg2rdf.ts
+++ src/gg2rdf.ts
@@ -216,7 +216,6 @@ function makeTreatment() {
       ? e
       : e.querySelector("taxonomicName");
     const citation = taxonConceptCitation(taxon, cTaxon);
-    console.log("found taxon citation; " + e.tagName, citation);
     if (citation) properties.push(citation);
   });
 
@@ -395,11 +394,14 @@ const enum REL {
 function getTaxonRelation(
   { taxon, cTaxon }: { taxon: Element; cTaxon: Element },
 ) {
-  const authorityMatch = (cTaxon.getAttribute("authorityYear") ===
-      taxon.getAttribute("authorityYear") &&
-    cTaxon.getAttribute("authorityName") ===
-      taxon.getAttribute("authorityName")) ||
-    (cTaxon.getAttribute("baseAuthorityYear") ===
+  const authorityMatch =
+    (cTaxon.hasAttribute("authorityYear") &&
+      cTaxon.getAttribute("authorityYear") ===
+        taxon.getAttribute("authorityYear") &&
+      cTaxon.getAttribute("authorityName") ===
+        taxon.getAttribute("authorityName")) ||
+    (cTaxon.hasAttribute("baseAuthorityYear") &&
+      cTaxon.getAttribute("baseAuthorityYear") ===
         taxon.getAttribute("baseAuthorityYear") &&
       cTaxon.getAttribute("baseAuthorityName") ===
         taxon.getAttribute("baseAuthorityName"));
