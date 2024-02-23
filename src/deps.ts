b5c774b962de6e4f4b8591263ff6b43d2a684312
nleanba <25827850+nleanba@users.noreply.github.com>
2024-02-23 12:32:48 +0100

    making it callable from within ather deno programs

--- src/deps.ts
+++ src/deps.ts
@@ -10,3 +10,11 @@ export {
 } from "https://deno.land/std@0.202.0/http/file_server.ts";
 
 export { existsSync } from "https://deno.land/std@0.202.0/fs/mod.ts";
+
+export { parseArgs } from "https://deno.land/std@0.215.0/cli/parse_args.ts";
+
+export { DOMParser } from "https://esm.sh/linkedom@0.16.8/cached";
+
+// broken somehow??
+// export { Element } from "https://esm.sh/v135/linkedom@0.16.8/types/interface/element.d.ts";
+// export type Element = globalThis.Element
