6af4f8fc7922e945dac255a96cd4dd8c65ffbbab
nleanba <25827850+nleanba@users.noreply.github.com>
2024-02-26 14:48:56 +0100

    updated constructor

--- src/gg2rdf.ts
+++ src/gg2rdf.ts
@@ -12,13 +12,8 @@ import { DOMParser, parseArgs } from "./deps.ts";
 import { Element } from "https://esm.sh/v135/linkedom@0.16.8/types/interface/element.d.ts";
 
 class Subject {
-  uri: string;
-  properties: { [key: string]: Set<string> };
-
-  constructor(uri: string) {
-    this.uri = uri;
-    this.properties = {};
-  }
+  properties: { [key: string]: Set<string> } = {};
+  constructor(public uri: string) {}
 
   /** sorted with comments first, and type last */
   get propNames() {
@@ -52,7 +47,6 @@ if (import.meta.main) {
 
   if (!flags.input) throw new Error("No input file provided");
   if (!flags.output) flags.output = flags.input + ".ttl";
-  console.log("aha");
   gg2rdf(flags.input, flags.output);
 }
 
