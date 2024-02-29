9f6ccf799307847879e2ddfbb5c9202a61379cca
nleanba <25827850+nleanba@users.noreply.github.com>
2024-02-29 09:11:06 +0100

    fixes #2

--- src/gg2rdf.ts
+++ src/gg2rdf.ts
@@ -233,6 +233,10 @@ export function gg2rdf(inputPath: string, outputPath: string) {
       }
     }
 
+    if (doc.hasAttribute("docTitle")) {
+      t.addProperty("dc:title", doc.getAttribute("docTitle"));
+    }
+
     t.addProperty(`dc:creator`, getAuthors());
     t.addProperty(`trt:publishedIn`, getPublication());
 
