ce43f56f802033f85d7d890b873b446055b6be37
nleanba <25827850+nleanba@users.noreply.github.com>
2024-05-27 14:27:06 +0200

    update dependency GHAct

--- src/action_worker.ts
+++ src/action_worker.ts
@@ -4,16 +4,13 @@
 * The jobs are accepted as messages and stored on disk, when the worker is started uncompleted jobs are picked up and exxecuted.
 
 */
-import GhactServiceWorker from "ghact/src/GhactServiceWorker.ts";
-import GitRepository from "ghact/src/GitRepository.ts";
-import { existsSync } from "./deps.ts";
+import { existsSync, GHActWorker, GitRepository, type Job } from "./deps.ts";
 import { config } from "../config/config.ts";
-import { type Job } from "ghact/src/JobsDataBase.ts";
 import { gg2rdf } from "./gg2rdf.ts";
 
 const GHTOKEN = Deno.env.get("GHTOKEN");
 
-const worker = new GhactServiceWorker(self, config, (job: Job, log) => {
+const worker = new GHActWorker(self, config, async (job: Job, log) => {
   try {
     log("Starting transformation\n" + JSON.stringify(job, undefined, 2));
 
@@ -22,19 +19,27 @@ const worker = new GhactServiceWorker(self, config, (job: Job, log) => {
     let message = "";
 
     const targetRepo = new GitRepository(
-      config.targetRepositoryUri, 
+      config.targetRepositoryUri,
       config.targetBranch,
       GHTOKEN,
-      config.workDir+"/target-repo")
+      config.workDir + "/target-repo",
+    );
 
-    if (job.files) {
-      modified = job.files.modified || [];
-      removed = job.files.removed || [];
+    if ("files" in job) {
+      modified = job.files.modified;
+      if ("added" in job.files) {
+        modified = [...modified, ...job.files.added];
+        removed = job.files.removed;
+      }
       message = `GG2RDF ${job.id} (${config.sourceRepository})`;
       //ghact should take care of this
       //updateLocalData("source", log); // also done by getModifiedAfter
     } else if (job.from) {
-      const files = worker.gitRepository.getModifiedAfter(job.from, job.till, log);
+      const files = await worker.gitRepository.getModifiedAfter(
+        job.from,
+        job.till,
+        log,
+      );
       modified = [...files.added, ...files.modified];
       removed = files.removed;
       if (files.till && files.till !== "HEAD") {
@@ -55,7 +60,7 @@ const worker = new GhactServiceWorker(self, config, (job: Job, log) => {
     for (const file of modified) {
       if (
         file.endsWith(".xml") &&
-        existsSync(`${worker.gitRepository.workDir}/${file}`)
+        existsSync(`${worker.gitRepository.directory}/${file}`)
       ) {
         Deno.mkdirSync(
           config.workDir + "/tmpttl/" + file.slice(0, file.lastIndexOf("/")),
@@ -65,7 +70,7 @@ const worker = new GhactServiceWorker(self, config, (job: Job, log) => {
         );
         try {
           gg2rdf(
-            `${worker.gitRepository.workDir}/${file}`,
+            `${worker.gitRepository.directory}/${file}`,
             `${config.workDir}/tmpttl/${file.slice(0, -4)}.ttl`,
             log,
           );
@@ -88,25 +93,21 @@ const worker = new GhactServiceWorker(self, config, (job: Job, log) => {
       if (file.endsWith(".xml")) {
         try {
           Deno.mkdirSync(
-            `${config.workDir}/target-repo/${
-              file.slice(0, file.lastIndexOf("/"))
-            }`,
+            `${targetRepo.directory}/${file.slice(0, file.lastIndexOf("/"))}`,
             {
               recursive: true,
             },
           );
           Deno.renameSync(
             `${config.workDir}/tmpttl/${file.slice(0, -4)}.ttl`,
-            `${config.workDir}/target-repo/${file.slice(0, -4)}.ttl`,
+            `${targetRepo.directory}/${file.slice(0, -4)}.ttl`,
           );
           // TODO check if newer?
         } catch (e) {
           log(
             `Failed to move ${config.workDir}/tmpttl/${
               file.slice(0, -4)
-            }.ttl to ${config.workDir}/target-repo/${
-              file.slice(0, -4)
-            }.ttl: \n${e}`,
+            }.ttl to ${targetRepo.directory}/${file.slice(0, -4)}.ttl: \n${e}`,
           );
         }
       }
@@ -114,9 +115,7 @@ const worker = new GhactServiceWorker(self, config, (job: Job, log) => {
 
     for (const file of removed) {
       if (file.endsWith(".xml")) {
-        const ttlFile = `${config.workDir}/target-repo/${
-          file.slice(0, -4)
-        }.ttl`;
+        const ttlFile = `${targetRepo.directory}/${file.slice(0, -4)}.ttl`;
         try {
           Deno.removeSync(ttlFile);
         } catch (e) {
@@ -127,43 +126,14 @@ const worker = new GhactServiceWorker(self, config, (job: Job, log) => {
       }
     }
 
-    const gitCommands = `git config --replace-all user.name ${job.author.name}
-    git config --replace-all user.email ${job.author.email}
-    git add -A
-    git commit --quiet -m "${message}"
-    git push --quiet ${
-      config.targetRepositoryUri.replace(
-        "https://",
-        `https://${GHTOKEN}@`,
-      )
-    }`;
-    const p = new Deno.Command("bash", {
-      args: [
-        "-c",
-        gitCommands,
-      ],
-      cwd: `${config.workDir}/target-repo`,
-    });
-    const { success, stdout, stderr } = p.outputSync();
-    if (!success) {
-      log("git push failed: ");
-    } else {
-      log("git push succesful:");
-    }
-    log("STDOUT:");
-    log(new TextDecoder().decode(stdout));
-    log("STDERR:");
-    log(new TextDecoder().decode(stderr));
-    if (!success) {
-      throw new Error("Abort.");
-    }
-    
+    await targetRepo.commit(job, message, log);
+    await targetRepo.push(log);
+    log("git push successful");
     log("Completed transformation successfully");
-    
   } catch (error) {
     log("FAILED TRANSFORMATION");
     log(error);
     if (error.stack) log(error.stack);
-    throw(error);
+    throw error;
   }
 });
