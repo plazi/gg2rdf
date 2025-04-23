8437bc172fea59e8cbea839f4aedad2313f72b61
nleanba <25827850+nleanba@users.noreply.github.com>
2025-04-23 20:55:17 +0200

    fixed dependecy

--- src/deps.ts
+++ src/deps.ts
@@ -10,7 +10,7 @@ export { existsSync } from "https://deno.land/std@0.224.0/fs/mod.ts";
 
 // used by gg2rdf.ts:
 export { parseArgs } from "https://deno.land/std@0.224.0/cli/parse_args.ts";
-export { DOMParser } from "https://esm.sh/linkedom@0.16.8/cached";
+export { DOMParser } from "npm:linkedom@0.16.8/cached";
 export { iso6393To1 } from "https://esm.sh/iso-639-3@3.0.1";
 
 // DOMParser, iso6393To1, parseArgs
